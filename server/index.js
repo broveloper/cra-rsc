988dd9e969d983ca0fd6d270816c02783d775f84
broveloper <broveloper@gmail.com>
2023-07-26 11:29:03 -0400

    Add serve script to package.json

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 3005, err => {
+app.listen(process.env.PORT || 3000, err => {
   if (err) {
     console.error(`Server start error`, err);
     process.exit();
